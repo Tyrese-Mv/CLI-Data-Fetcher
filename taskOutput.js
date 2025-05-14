ba31e6d0d0e9bab07dc70e7cec3a4328a07a2d79
Tyrese-Mv <musawenkosi106@gmail.com>
2025-05-14 14:58:07 +0200

    documented taskOutput function

--- taskOutput.js
+++ taskOutput.js
@@ -1,3 +1,10 @@
+/**
+ * Outputs formatted task details to the console.
+ *
+ * @param {Object} obj - The task object to display.
+ * @param {string} obj.title - The title or description of the task.
+ * @param {boolean} obj.completed - Indicates whether the task is completed
+ */
 function outputTask(obj){
     console.log("\n")
     console.log(`Task: ${obj.title}`);
